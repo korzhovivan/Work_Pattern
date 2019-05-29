1873141bfea8e00fca7603c6e13a6c1188583370
ko <korzhoff.i@gmail.com>
2019-05-29 23:36:29 +0300

    commit

---
+++ ConsoleApp11/Camera.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp11
+{
+    internal class Camera
+    {
+        public static int counter { get; set; } = 0;
+        private static Dictionary<int, Camera> dict = new Dictionary<int, Camera>();
+
+        public Camera() {}
+
+        public static void Add(int ip, Camera cam)
+        {
+            counter++;
+            if (counter > 3)
+            {
+                throw new ArgumentException("lol");
+            }
+            else
+            {
+                dict.Add(ip, cam);
+                
+            }
+
+        }
+        public static Camera GetInstance(int ipadress)
+        {
+            if (dict.ContainsKey(ipadress))
+            {
+                return dict[ipadress];
+            }
+            else
+            {
+                if (counter < 3)
+                {
+                    dict.Add(counter, new Camera());
+                    counter++;
+                    return new Camera();
+                }
+                else
+                {
+                    throw new ArgumentException("lol");
+                }
+            }
+        }
+    }
+}
---
+++ ConsoleApp11/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Camera cam1 = new Camera();
+            Camera cam2 = new Camera();
+            Camera cam3 = new Camera();
+            Camera cam4 = new Camera();
+
+            Camera.Add(101, cam1);
+            Console.WriteLine(Camera.counter);
+            Camera.Add(102, cam2);
+            Console.WriteLine(Camera.counter);
+            Camera.Add(103, cam3);
+            Console.WriteLine(Camera.counter);
+            //Camera.Add(104, cam4);
+            //Console.WriteLine(Camera.counter);
+            
+
+            Console.ReadKey();
+                Console.WriteLine(Camera.counter);
+        }
+    }
+}
